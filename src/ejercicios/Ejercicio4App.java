9696c315d622fc05fbfd0cba14dce27954c25954
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-09 19:33:22 +0200

    ejercicios del 1 al 5

---
+++ src/ejercicios/Ejercicio4App.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Ejercicio4App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		System.out.println("Valor inicial: " + n);
+		n = n + 77;
+		System.out.println("Incremento en 77 posiciones: " + n);
+		n = n - 3;
+		System.out.println("Decremento en 3 posiciones: " + n);
+		n = n*2;
+		System.out.println("Valor duplicado: " + n);
+	}
+	
+}
